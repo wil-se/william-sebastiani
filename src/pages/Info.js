601eceff4cbeb0bb46a7f7ed364cfbcee9d06b0d
William <sebastiani.1753672@studenti.uniroma1.it>
2022-05-20 20:37:59 +0200

    fix

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,7 +1,7 @@
 import { Col, Row } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import Markdown from "markdown-to-jsx";
-import ProfilePic from '../assets/img/william-sebastiani.jpg'
+import ProfilePic from '../assets/img/william-nobg.png'
 
 const Info = (props) => {
     const [info, setInfo] = useState("");
@@ -23,7 +23,6 @@ const Info = (props) => {
         <Row>
           <div className="article-title"><h2>Chi sono</h2></div>
           <div className="article-body">
-            <div className="text-center" style={{marginBottom: 10}}><img src={ProfilePic} style={{height: 256, width: 256}}/></div>
             <Markdown>{info}</Markdown>
           </div>
         </Row>
